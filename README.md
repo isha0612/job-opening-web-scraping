861a4c79bff3404d9d13dd199b6437b04c2ef7b8
isha0612 <isha06dec@gmail.com>
2024-03-05 22:49:55 +0530

    impovise the csv

--- README.md
+++ README.md
@@ -4,11 +4,20 @@
 This web scraping project aims to gather information on the latest job opportunities from approximately 10 companies or startups and organizes the data into a CSV file.
 
 
-## Clone the repository, install packages and run the application
+### Clone the repository, install packages
+
 ```terminal
 $ git clone https://github.com/isha0612/job-opening-web-scraping.git
 $ npm i  
+```
+
+### Add your email and password of your wellfound account in the .env file and run the application
+
+```terminal
+$ echo "EMAIL=YOUR_EMAIL" >> src/.env
+$ echo "PASSWORD=YOUR_PASSWORD" >> src/.env
 $ npm run dev
 ```
+
 
 This  will create an output.csv file that holds the accumulated data.
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
@@ -6,6 +7,7 @@ const { URL } = require('url');
 
 puppeteer.use(StealthPlugin());
 
+const loginUrl = 'https://wellfound.com/login';
 const url = 'https://wellfound.com/discover/startups?location=bangalore-urban';
 
 const launchOptions = {
@@ -25,7 +27,16 @@ const csvWriter = createCsvWriter({
         { id: 'fund', title: 'Company Funding' },
         { id: 'location', title: 'Locations' },
         { id: 'market', title: 'Markets' },
-        { id: 'job', title: 'Job Openings' },
+        { id: 'job1_position', title: 'Job-1 Position' },
+        { id: 'job1_department', title: 'Job-1 Department' },
+        { id: 'job1_description', title: 'Job-1 Description' },
+        { id: 'job1_location', title: 'Job-1 Location' },
+        { id: 'job1_salary', title: 'Job-1 Salary' },
+        { id: 'job2_position', title: 'Job-2 Position' },
+        { id: 'job2_department', title: 'Job-2 Department' },
+        { id: 'job2_description', title: 'Job-2 Description' },
+        { id: 'job2_location', title: 'Job-2 Location' },
+        { id: 'job2_salary', title: 'Job-2 Salary' },
     ],
 });
 
@@ -36,141 +47,122 @@ async function scrapeJobOpenings() {
     await page.setViewport({ width: 1280, height: 720 });
 
     try {
-        await page.goto(url, { waitUntil: 'domcontentloaded' });  // Increase the timeout as needed
-        const limitedLists = await page.$$("a[href^='/company/']");
-        console.log(limitedLists.length);
-        const companyLists = new Array();
-        let i = 0;
+        await page.goto(loginUrl, { waitUntil: 'domcontentloaded' });
+        // const emailField = await page.$('input[type="email"]');
+        await page.waitForSelector('input[type="email"]');
+        const emailField = await page.$('input[type="email"]');
+        await page.waitForSelector('input[type="password"]');
+        const passwordField = await page.$('input[type="password"]');
+        await page.waitForSelector('input[type="submit"]');
+        const loginField = await page.$('input[type="submit"]');
 
-        for (const company of limitedLists) {
-            companyLists.push(await company.evaluate(element => element.getAttribute('href')));
-            i++;
-            if(i === 10) break;
-        }
+        if (emailField && passwordField && loginField) {
+            // Type some text into the email field
+            await emailField.type(process.env.EMAIL);
+            await passwordField.type(process.env.PASSWORD);
+            await loginField.click();
 
-        for (const href of companyLists) {
-            try {
-                console.log('href:', href);
+            await page.waitForNavigation();
 
-                const match = href.match(/\/company\/(.*)/);
-                const completeUrl = new URL(`${href}/jobs`, url).href;
-                await page.goto(completeUrl, { waitUntil: 'domcontentloaded' });
-                console.log('Navigated to:', completeUrl);
+            // Perform other interactions as needed
+            await page.goto(url, { waitUntil: 'domcontentloaded' });  // Increase the timeout as needed
+            await page.waitForSelector("a[href^='/company/']");
+            const limitedLists = await page.$$("a[href^='/company/']");
+            console.log(limitedLists.length);
+            const companyLists = new Array();
 
-                const h2 = await page.$('h2');
-                const dts = await page.$$('dt');
-                const locationhrefs = await page.$$("a[href^='/startups/location/']");
-                const marketrefs = await page.$$("a[href^='/startups/industry/']");
-                const job = await page.$$('.styles_component__2UhSH');
-
-                const companyName = match[1];
-                console.log("companyName", companyName);
-
-                const companyDescription = await h2.evaluate(element => element.textContent);
-                console.log("companyDescription", companyDescription);
-
-                const companySize = await dts[2].evaluate(element => element.textContent);
-                console.log("companySize", companySize);
-
-                const companyFunding = await dts[3].evaluate(element => element.textContent);
-                console.log("companyFunding", companyFunding);
-
-                const companyLocations = new Set();
-                for (let locationhref of locationhrefs) {
-                    companyLocations.add(await locationhref.evaluate(element => element.textContent));
-                }
-                console.log("companyLocations", companyLocations);
-
-                const companyMarkets = new Set();
-                for (let marketref of marketrefs) {
-                    companyMarkets.add(await marketref.evaluate(element => element.textContent));
-                }
-                console.log("companyMarkets", companyMarkets);
-
-                const postions = new Set();
-                const departments = new Set();
-                const jobDescriptions = new Set();
-                const jobLocations = new Set();
-                const jobSalaries = new Set();
-                const jobInfo = new Array();
-
-                for (let i = 0; i < 2; i++) {
-                    const position = await job[i].evaluate(element => element ? element.querySelector('h4').textContent : null);
-                    // if(position === null) {
-                    //     position = "Not Available";
-                    // }
-                    postions.add(position);
-                    console.log("position", position);
-
-                    const department = await job[i].evaluate(element => element ? element.querySelector('h6').textContent : null);
-                    // if(department === null) {
-                    //     position = "Not Available";
-                    // }
-                    departments.add(department);
-                    console.log("department", department);
-
-                    const jobDescription = await job[i].evaluate(element => element ? element.querySelector('.styles_descriptionSnippet__j1vlH').textContent : null);
-                    // if(department === null) {
-                    //     position = "Not Available";
-                    // }
-                    jobDescriptions.add(jobDescription);
-                    console.log("jobDescription", jobDescription);
-
-                    const jobLocation = await job[i].evaluate(element => element ? element.querySelector('.line-clamp-2').textContent : null);
-                    // if(jobLocation === null) {
-                    //     position = "Not Available";
-                    // }
-                    jobLocations.add(jobLocation);
-                    console.log("jobLocation", jobLocation);
-
-                    const jobSalary = await job[i].evaluate(element => element ? element.querySelector('.styles_compensation__DUzmb').textContent : null);
-                    // if(jobSalary === null) {
-                    //     position = "Not Available";
-                    // }
-                    jobSalaries.add(jobSalary);
-                    console.log("jobSalary", jobSalary);
-
-                    jobInfo.push({
-                        position,
-                        department,
-                        jobDescription,
-                        jobLocation,
-                        jobSalary
-                    });
-                }
-
-                const data = [{
-                    name: companyName,
-                    about: companyDescription,
-                    size: companySize,
-                    fund: companyFunding,
-                    location: Array.from(companyLocations).join(', '),
-                    market: Array.from(companyMarkets).join(', '),
-                    job: JSON.stringify(jobInfo)
-                }];
-
-                csvWriter
-                    .writeRecords(data)    // returns a promise
-                    .then(() => {
-                        console.log('...Done');
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
+            for (let company of limitedLists) {
+                companyLists.push(await company.evaluate(element => element.getAttribute('href')));
             }
-            catch (error) {
-                continue;
+
+            let i = 0;
+            for (const href of companyLists) {
+                try {
+                    console.log('href:', href);
+
+                    const match = href.match(/\/company\/(.*)/);
+                    const completeUrl = new URL(`${href}/jobs?location=bangalore-urban`, url).href;
+                    await page.goto(completeUrl, { waitUntil: 'domcontentloaded' });
+                    console.log('Navigated to:', completeUrl);
+
+                    const h2 = await page.$('h2');
+                    const dts = await page.$$('dt');
+                    const locationhrefs = await page.$$("a[href^='/startups/location/']");
+                    const marketrefs = await page.$$("a[href^='/startups/industry/']");
+                    const job = await page.$$('.styles_component__2UhSH');
+
+                    const companyName = match[1];
+                    console.log("companyName", companyName);
+
+                    const companyDescription = await h2.evaluate(element => element.textContent);
+                    console.log("companyDescription", companyDescription);
+
+                    const companySize = await dts[2].evaluate(element => element.textContent);
+                    console.log("companySize", companySize);
+
+                    const companyFunding = await dts[3].evaluate(element => element.textContent);
+                    console.log("companyFunding", companyFunding);
+
+                    const companyLocations = new Set();
+                    for (let locationhref of locationhrefs) {
+                        companyLocations.add(await locationhref.evaluate(element => element.textContent));
+                    }
+                    console.log("companyLocations", companyLocations);
+
+                    const companyMarkets = new Set();
+                    for (let marketref of marketrefs) {
+                        companyMarkets.add(await marketref.evaluate(element => element.textContent));
+                    }
+                    console.log("companyMarkets", companyMarkets);
+
+                    const jobPosition1 = await job[0].evaluate(element => element ? element.querySelector('h4').textContent : null);
+                    const jobPosition2 = await job[1].evaluate(element => element ? element.querySelector('h4').textContent : null);
+
+                    const jobDepartment1 = await job[0].evaluate(element => element ? element.querySelector('h6').textContent : null);
+                    const jobDepartment2 = await job[1].evaluate(element => element ? element.querySelector('h6').textContent : null);
+
+                    const jobLocation1 = await job[0].evaluate(element => element ? element.querySelector('.line-clamp-2').textContent : null);
+                    const jobLocation2 = await job[1].evaluate(element => element ? element.querySelector('.line-clamp-2').textContent : null);
+
+                    const jobDescription1 = await job[0].evaluate(element => element ? element.querySelector('.styles_descriptionSnippet__j1vlH').textContent : null);
+                    const jobDescription2 = await job[1].evaluate(element => element ? element.querySelector('.styles_descriptionSnippet__j1vlH').textContent : null);
+
+                    const jobSalary1 = await job[0].evaluate(element => element ? element.querySelector('.styles_compensation__DUzmb').textContent : null);
+                    const jobSalary2 = await job[1].evaluate(element => element ? element.querySelector('.styles_compensation__DUzmb').textContent : null);
+                    
+                    const data = [{
+                        name: companyName,
+                        about: companyDescription,
+                        size: companySize,
+                        fund: companyFunding,
+                        location: Array.from(companyLocations).join(', '),
+                        market: Array.from(companyMarkets).join(', '),
+                        job1_position: jobPosition1,
+                        job1_department: jobDepartment1,
+                        job1_description: jobDescription1,
+                        job1_location: jobLocation1,
+                        job1_salary: jobSalary1,
+                        job2_position: jobPosition2,
+                        job2_department: jobDepartment2,
+                        job2_description: jobDescription2,
+                        job2_location: jobLocation2,
+                        job2_salary: jobSalary2
+                    }];
+
+                    await csvWriter.writeRecords(data);
+                    i++;
+                    if (i === 10) break;
+                }
+                catch (error) {
+                    continue;
+                }
             }
         }
+
+
 
     } catch (error) {
-        if (error.message.includes('Protocol error (Runtime.callFunctionOn): Could not find object with given id')) {
-            console.error('Caught a Protocol error, retrying...');
-            // Retry or handle accordingly
-
-        } else {
-            throw error; // Re-throw other errors
-        }
+        console.error('error:', error);
     } finally {
         await browser.close();
     }
